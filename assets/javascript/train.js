3a83b8f06cf3f53590d7b56dd957b735abf5ec3c
lasperilla <l.asperilla@gmail.com>
2017-03-26 11:23:56 -0400

    created files, directories. html css, dummy schedule table

---
+++ assets/javascript/train.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyASp6PyVpnCB8OEGFualB5X4R1hvpODyBM",
+	authDomain: "train-scheduler-efd8e.firebaseapp.com",
+	databaseURL: "https://train-scheduler-efd8e.firebaseio.com",
+	storageBucket: "train-scheduler-efd8e.appspot.com",
+	messagingSenderId: "580217922699"
+};
+
+firebase.initializeApp(config);
+// End Initialize Firebase
+
